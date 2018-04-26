8cc49afb0d3d9fa4b6cbc8f4926bafc79de86336
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-26 13:42:07 +0200

    creation page create_user

---
+++ site-backend/app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def new_post
+  end
+
+end
--- site-backend/config/routes.rb
+++ site-backend/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#index'
+  get 'create_user', to: 'users#new', as: 'creation'
+  post 'create_user', to: 'users#new_post', as: 'creation_post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
