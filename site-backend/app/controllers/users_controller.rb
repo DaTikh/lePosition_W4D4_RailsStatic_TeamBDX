a7b855fb0deb08a051dd578abf9e614cef8a5a33
iMassim0 <i.max@live.fr>
2018-04-26 14:56:33 +0200

    MAJ du site backend - login restriction erreur donctionnels

--- site-backend/app/controllers/users_controller.rb
+++ site-backend/app/controllers/users_controller.rb
@@ -4,6 +4,44 @@ class UsersController < ApplicationController
   end
 
   def new_post
+
+    puts 'Console ma caille'
+
+    @user = User.new
+
+    @user.username = params[:username]
+
+    @user.bio = params[:bio]
+
+    @user.save
+
+    puts @user.errors.class
+
+    if @user == User.last
+
+      redirect_to user_path"#{@user.id}"
+
+    else
+
+      puts "Erreur d'entrée !"
+
+      redirect_to error_path
+
+    end
+
+  end
+
+
+  def show
+
+    @user = User.find(params[:use])
+
+  end
+
+  def error
+
+
+
   end
 
 end
--- site-backend/app/models/user.rb
+++ site-backend/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+validates :username, presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+
 end
