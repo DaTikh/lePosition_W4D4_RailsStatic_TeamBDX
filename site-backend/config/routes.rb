24a50070e8ed26c1b06e65b113fb7fd926e32285
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-26 18:54:45 +0200

    fix routing error

--- site-backend/config/routes.rb
+++ site-backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#index'  # Définition de la page d'accueil
   get 'create_user', to: 'users#new', as: 'creation'  # Routing de la page création de profil sur le controller Users
   post 'create_user', to: 'users#new_post', as: 'creation_post'  # Lors de l'envoi du formulaire
-  get 'users(/:use)', to: "users#show", as: 'user'  # Routing du profil utilisateur
-  get '/error', to: "users#error", as: 'error'  # Routing du message d'erreur
+  get 'users/error', to: "users#error", as: 'error'  # Routing du message d'erreur
+  get 'users(/:username)', to: "users#show", as: 'user'  # Routing du profil utilisateur
 
 end
